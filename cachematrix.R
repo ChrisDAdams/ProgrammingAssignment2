1bd370512484a9c185512613ffdc1cebd0e2c006
Chris <cadams8658@aol.com>
2014-09-19 08:45:42 -0400

    first submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+############################################################
+# Name: makeMatrix.R                                       #
+# Function: makeCacheMatrix                                #
+# Parm: Matrix                                             #
+# Date: Sept 19,2014                                       #
+# Comment: Cloned from makeCacheMean Sample                #
+############################################################
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Initialize local variable
+  m <- NULL
+  
+  # set function used to replace cached maatrix 
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  # get function returns matrix
+  get <- function() x
+  
+  # setSolve fun ction applies inverse (solve)
+  setSolve <- function(solve) m <<- solve
+  
+  # getSolve function that returns local variable
+  getSolve <- function() m
+  
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+############################################################
+# Name: cachesolve.R                                       #
+# Function: cachesolve                                     #
+# Parm: Matrix from makeCacheMatrix Function               #
+# Date: Sept 19,2014                                       #
+# Comment: Cloned from cacheMean Sample                    #
+############################################################
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  # If cached matrix exist return the cached matrix
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # Else get the matrix and apply inverse function (solve)
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  # Return inverse matrix
+  m
 }
